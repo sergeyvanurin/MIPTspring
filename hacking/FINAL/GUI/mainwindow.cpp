db9c25632bfea9fddc14c287611b20faf68bc0ae
Sergey Vanurin <vanurin.sm@phystech.edu>
2020-05-20 17:50:51 +0300

    minor

--- hacking/FINAL/GUI/mainwindow.cpp
+++ hacking/FINAL/GUI/mainwindow.cpp
@@ -30,9 +30,9 @@ void MainWindow::on_File_choose_button_clicked()
     if (file_dialog.exec())
     {
         filename = file_dialog.selectedFiles()[0];
+        ui->adress_bar->setText(filename);
+        ui->Patch_button->setEnabled(true);
     }
-    ui->adress_bar->setText(filename);
-    ui->Patch_button->setEnabled(true);
 }
 
 void MainWindow::on_Patch_button_clicked()
@@ -43,28 +43,29 @@ void MainWindow::on_Patch_button_clicked()
         ui->label_under_button->setStyleSheet("QLabel {color:rgb(255,0,0)}");
         ui->label_under_button->setText("Error opening a file :(");
     }
+
     else
     {
         ui->label_under_button->setStyleSheet("QLabel {color:rgb(0,255,0)}");
         ui->label_under_button->setText("Successfuly patched");
+        gif_window = new QWidget;
+        this->hide();
+        QMovie* movie = new QMovie("/Users/vanur/Downloads/hackerman.gif");
+        if (!movie->isValid())
+        {
+            this->show();
+        }
+        else
+        {
+            movie_label = new QLabel(gif_window);
+            movie_label->setGeometry(0, 0, 480, 270);
+            movie_label->setMovie(movie);
+            movie->start();
+            gif_window->show();
+
+        }
     }
 
-    gif_window = new QWidget;
-    this->hide();
-    QMovie* movie = new QMovie("/Users/vanur/Downloads/hackerman.gif");
-    if (!movie->isValid())
-    {
-        this->show();
-    }
-    else
-    {
-        movie_label = new QLabel(gif_window);
-        movie_label->setGeometry(0, 0, 480, 270);
-        movie_label->setMovie(movie);
-        movie->start();
-        gif_window->show();
-
-    }
 }
 
 int MainWindow::Patcher(std::string filename)
@@ -77,11 +78,11 @@ int MainWindow::Patcher(std::string filename)
         return 1;
     }
 
-    unsigned int first_byte_to_patch = 0x33;
-    char first_patch_value = 0x74;
+    const unsigned int first_byte_to_patch = 0x33;
+    const char first_patch_value = 0x74;
 
-    unsigned int second_byte_to_patch = 0x54;
-    char second_patch_value[2] = {(char)0xAC, (char)0xBC};
+    const unsigned int second_byte_to_patch = 0x54;
+    const char second_patch_value[2] = {(const char)0xAC, (const char)0xBC};
 
     fseek(binary_executable, first_byte_to_patch, SEEK_SET);
     fwrite(&first_patch_value, 1, 1, binary_executable);
