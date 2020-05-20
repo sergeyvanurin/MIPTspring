f0e00d7f75a369bf9f251a5a46c5b65d355af581
Sergeyvanurin <sergeyvanurin@protonmail.com>
2020-05-20 17:09:15 +0300

    Update strchr.asm

--- asm/FUNCTION/strchr.asm
+++ asm/FUNCTION/strchr.asm
@@ -4,12 +4,12 @@ org 100h
 
 
 start:
-		lea di, msg
-		mov al, 'Z'
-		call strchr
-		
-		mov ax, 4c00h
-		int 21h
+        lea di, msg
+        mov al, 'Z'
+        call strchr
+        
+        mov ax, 4c00h
+        int 21h
      
 ;====================================================================================
 ;char* strchr (char* str, unsigned char character);
@@ -20,27 +20,27 @@ start:
 ;returns:di - pointer to a first occurrence of character in str. NULL if not found
 ;====================================================================================
 strchr:
-		cld
+        cld
 
 search_loop:
         cmp byte ptr ds:[di], 0h
-		je not_found
+        je not_found
 
-		scasb
-		je found
+        scasb
+        je found
 
-		jmp search_loop
+        jmp search_loop
 
 not_found:
-		xor di, di
-		ret
+        xor di, di
+        ret
 
 found:
-		dec di
-		ret
+        dec di
+        ret
 
 
 msg db 'PIZZA', 0h
 
 end start
-	
+    
