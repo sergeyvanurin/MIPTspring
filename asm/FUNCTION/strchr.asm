6b0faa03b35295d30e31a7a1fcced3d943ebe156
Sergeyvanurin <sergeyvanurin@protonmail.com>
2020-05-20 17:06:53 +0300

    minor

--- asm/FUNCTION/strchr.asm
+++ asm/FUNCTION/strchr.asm
@@ -16,31 +16,33 @@ start:
 ;di - str
 ;al - character
 ;
-;dest:
+;dest: di
 ;returns:di - pointer to a first occurrence of character in str. NULL if not found
 ;====================================================================================
 strchr:
 		cld
+		cmp byte ptr ds:[di], 0h        ;if di is empty
+		jne not_found
 
 search_loop:
 		scasb
 		je found
 
-		cmp byte ptr ds:[di], 0dh
+		cmp byte ptr ds:[di], 0h
 		je not_found
 
 		jmp search_loop
 
 not_found:
-
 		xor di, di
 		ret
+
 found:
 		dec di
 		ret
 
 
-msg db 'PIZZA', 0dh
+msg db 'PIZZA', 0h
 
 end start
 	
